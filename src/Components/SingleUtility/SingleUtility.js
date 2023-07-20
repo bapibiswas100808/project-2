6ea9f9720b7b086185111250c2bf547b5e9457ea
bapibiswas100808 <star.bappy@gmail.com>
2023-07-20 13:29:18 +0600

    project modified

--- src/Components/SingleUtility/SingleUtility.js
+++ src/Components/SingleUtility/SingleUtility.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { Col } from "react-bootstrap";
 import "./SingleUtility.css";
 
 const SingleUtility = ({ item }) => {
   return (
-    <Col lg={3}>
-      <div className="single-utility">
-        <div className="utility-image mb-4">
-          <img src={item.image} alt="" className="w-100" />
-        </div>
-        <div className="utility-text">
-          <h3 className="text-white pb-2">{item.heading}</h3>
-          <p className="text-white">{item.text}</p>
-        </div>
+    // <Col lg={3}>
+    <div className="single-utility">
+      <div className="utility-image mb-4">
+        <img src={item.image} alt="" className="w-100" />
       </div>
-    </Col>
+      <div className="utility-text">
+        <h3 className="text-white pb-2">{item.heading}</h3>
+        <p className="text-white">{item.text}</p>
+      </div>
+    </div>
+    // </Col>
   );
 };
 
--- src/Components/Utility/Utility.js
+++ src/Components/Utility/Utility.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Row } from "react-bootstrap";
-import SingleUtility from "../SingleUtility/SingleUtility";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import "./Utility.css";
+import SingleUtility from "../SingleUtility/SingleUtility";
 
 const Utility = () => {
   const [utility, setUtility] = useState([]);
@@ -10,15 +11,38 @@ const Utility = () => {
       .then((res) => res.json())
       .then((data) => setUtility(data));
   }, []);
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
     <section className="utility-area">
       <div className="project-sketch">
-        <h1 className="text-center text-white pb-3">HOLDERS UTILITY</h1>
-        <Row>
+        <Carousel
+          swipeable={true}
+          draggable={false}
+          showDots={true}
+          responsive={responsive}
+          containerClass="carousel-container"
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+        >
           {utility.map((item) => (
             <SingleUtility key={item.id} item={item}></SingleUtility>
           ))}
-        </Row>
+        </Carousel>
+        ;
       </div>
     </section>
   );
